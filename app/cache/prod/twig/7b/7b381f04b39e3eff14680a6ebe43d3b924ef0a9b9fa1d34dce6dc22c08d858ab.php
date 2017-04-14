e162deed02bae84b8fcbc8be129446cc8c0de07b
Antoine Gunzburger <antoine@phantombuster.com>
2017-04-14 13:20:00 -0700

    Once again

---
+++ app/cache/prod/twig/7b/7b381f04b39e3eff14680a6ebe43d3b924ef0a9b9fa1d34dce6dc22c08d858ab.php
@@ -0,0 +1,34 @@
+<?php
+
+/* NelmioApiDocBundle::Components/motd.html.twig */
+class __TwigTemplate_b66f36932fb5a8b8fc869c243911e4195cce555f7a4fe8d9209efcc61511abfe extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div class=\"motd\"></div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "NelmioApiDocBundle::Components/motd.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <div class="motd"></div>*/
+/* */
